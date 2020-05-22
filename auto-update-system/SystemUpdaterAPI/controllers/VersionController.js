a154d07497d0ab14677f023893972af2a1b366d3
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2020-05-21 22:54:50 -0300

    added: Donwload version Endpoint

--- auto-update-system/SystemUpdaterAPI/controllers/VersionController.js
+++ auto-update-system/SystemUpdaterAPI/controllers/VersionController.js
@@ -1,9 +1,8 @@
 module.exports = (app) => {
+    const fs = require("fs");
 
     // [GET] => /version
     app.get(`/version`, (req, res) => {
-        const fs = require("fs");
-
         let ver = null;
 
         const packages = fs.readdirSync(`${ROOT}/versions`);
@@ -25,5 +24,19 @@ module.exports = (app) => {
         res.json(ver);
     });
 
+    // [GET] => /version/:version
+    app.get(`/version/:version`, (req, res) => {
+        const { version } = req.params;
+
+        const packages = fs.readdirSync(`${ROOT}/versions`);
+        const file = `${ROOT}/versions/${version}/program.exe`;
+
+        if (!packages.includes(version) || !fs.existsSync(file)) {
+            res.status(404).json("Version not found!");
+            return;
+        }
+
+        res.download(file);
+    });
 
 };
